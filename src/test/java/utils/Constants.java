5258260c0e9796a712e405fbae708536a3c0c073
AlenaBu <abubenshchykava@gmail.com>
2022-03-30 23:03:17 -0400

    Latest code03302022

---
+++ src/test/java/utils/Constants.java
@@ -0,0 +1,8 @@
+package utils;
+
+public class Constants {
+    // for all frameworks and computers
+
+    public static final String CONFIGURATION_FILEPATH = System.getProperty("user.dir")+"/Users/lenka/TestNgFramework/src/test/resources/config/config.properties";
+    public static final int IMPLICIT_WAIT = 10;
+}
